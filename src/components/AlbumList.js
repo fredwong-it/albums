a8b20e6b2114044ad57137e580c9e9d30cea883f
Fred Wong <fredwong.it@gmail.com>
2017-07-14 09:59:36 -0400

    Add Header and AlbumList component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+//const AlbumList = () => {
+class AlbumList extends Component {
+  // initalize data loading process
+  componentWillMount() {
+
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
