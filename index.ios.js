22d23d0f51c4d3afb064ca1f622e5848c7a2259e
Fred Wong <fredwong.it@gmail.com>
2017-07-13 21:31:48 -0400

    Init project, add header

---
+++ index.ios.js
@@ -0,0 +1,20 @@
+// Index.ios.js - place code in here for IOS!!!!
+
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a Component
+// const App = () => {
+//   return (
+//     <Text>Some Text</Text>
+//   );
+// };
+
+const App = () => (
+    <Header />
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
