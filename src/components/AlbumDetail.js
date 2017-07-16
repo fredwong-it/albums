4189224b89c5e3aa53070b6683465f51594486a5
Fred Wong <fredwong.it@gmail.com>
2017-07-16 19:20:05 -0400

    Add Button component with proper style

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const {
@@ -32,6 +33,9 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
       <CardSection>
         <Image style={imageStyle} source={{ uri: image }}/>
+      </CardSection>
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   );
