7a3dd9af190e09292a085d0004bd1ecc31213b47
Fred Wong <fredwong.it@gmail.com>
2017-07-16 23:52:23 -0400

    Add click event to the Button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
@@ -9,7 +9,8 @@ const AlbumDetail = ({ album }) => {
     title,
     artist,
     image,
-    thumbnail_image
+    thumbnail_image,
+    url
   } = album;
   const {
     headerContentStyle,
@@ -35,7 +36,9 @@ const AlbumDetail = ({ album }) => {
         <Image style={imageStyle} source={{ uri: image }}/>
       </CardSection>
       <CardSection>
-        <Button />
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
 
+
   return (
-    <TouchableOpacity style={buttonStyle}>
+    <TouchableOpacity
+      style={buttonStyle}
+      onPress={onPress}
+    >
       <Text style={textStyle}>
-        Click me!!!
+        {children}
       </Text>
     </TouchableOpacity>
   );
